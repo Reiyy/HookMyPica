61d6f5f3482a05dbb1c0c4d8420d898e6317809a
夜棂依 <zhzhu722@reiyy.com>
2024-10-05 00:09:02 +0800

    发布：2.2.1.3.3.4-7.1
    2.2.1.3.3.4-7.1:
    第一版，
    取消移除首页广告，
    保留移除游戏标签页，
    添加跳过分流选择页面，
    自动选择分流一，
    修改图标颜色，

---
+++ hook/src/main/java/com/yareiy/hookmypica/MainHook.java
@@ -0,0 +1,119 @@
+package com.yareiy.hookmypica;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+@SuppressWarnings("RedundantThrows")
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("handleLoadPackage: " + lpparam.processName + ", " + lpparam.processName);
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.utils.views.PopupWebview",
+                lpparam.classLoader,
+                "init",
+                Context.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("beforeHookedMethod: PopupWebview.init(Context)");
+                        param.setResult(null);
+                    }
+                });
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.utils.views.BannerWebview",
+                lpparam.classLoader,
+                "init",
+                Context.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("beforeHookedMethod: BannerWebview.init(Context)");
+                        param.setResult(null);
+                    }
+                });
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.activities.MainActivity",
+                lpparam.classLoader,
+                "onCreate",
+                Bundle.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("afterHookedMethod: MainActivity.onCreate(Bundle)");
+                        // 取消首页广告移除，保留移除游戏选项
+                        View[] buttons_tabbar = (View[]) XposedHelpers.getObjectField(param.thisObject, "buttons_tabbar");
+                        buttons_tabbar[2].setVisibility(View.GONE);
+                    }
+                });
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.adapters.ComicPageRecyclerViewAdapter",
+                lpparam.classLoader,
+                "onCreateViewHolder",
+                ViewGroup.class,
+                int.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("afterHookedMethod: ComicPageRecyclerViewAdapter.onCreateViewHolder");
+                        Object result = param.getResult();
+                        if (!TextUtils.equals(result.getClass().getName(), "com.picacomic.fregata.holders.AdvertisementListViewHolder")) {
+                            return;
+                        }
+                        View webView_ads = (View) XposedHelpers.getObjectField(result, "itemView");
+                        webView_ads.setVisibility(View.GONE);
+                        Object lp = XposedHelpers.newInstance(XposedHelpers.findClass("android.support.v7.widget.RecyclerView$LayoutParams", lpparam.classLoader), 0, 0);
+                        webView_ads.setLayoutParams((ViewGroup.LayoutParams) lp);
+                    }
+                });
+        XposedHelpers.findAndHookMethod("com.picacomic.fregata.adapters.ComicListRecyclerViewAdapter", lpparam.classLoader, "onBindViewHolder", XposedHelpers.findClass("android.support.v7.widget.RecyclerView$ViewHolder", lpparam.classLoader), int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Object viewHolder = param.args[0];
+                if (viewHolder.getClass().getSimpleName().equals("AdvertisementListViewHolder")) {
+                    param.setResult(null);
+                    View itemView = (View) XposedHelpers.getObjectField(viewHolder, "itemView");
+                    ViewGroup.LayoutParams lp = (ViewGroup.LayoutParams) itemView.getLayoutParams();
+                    // 完全隐藏会影响分页加载的逻辑，所以保留一点，
+                    lp.height = 1;
+                    itemView.setLayoutParams(lp);
+                }
+            }
+        });
+        // 跳过分流选择页面并自动选择分流1
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.activities.SplashActivity",
+                lpparam.classLoader,
+                "onCreate",
+                Bundle.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        Object splashActivity = param.thisObject;
+
+                        // 使用反射查找 Button 类
+                        Class<?> buttonClass = XposedHelpers.findClass("android.widget.Button", lpparam.classLoader);
+                        
+                        // 获取分流1按钮
+                        Object button_server1 = XposedHelpers.getObjectField(splashActivity, "button_server1");
+
+                        // 点击分流1
+                        XposedHelpers.callMethod(button_server1, "performClick");
+
+                        // 结束 SplashActivity
+                        XposedHelpers.callMethod(splashActivity, "finish");
+                    }
+                });
+    }
+}
